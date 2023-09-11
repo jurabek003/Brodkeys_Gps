9008e389c96603f7de14d9bf1289b47ebdeed413
Asus <turgunboyevjurabek777@gmail.com>
2023-09-11 15:56:09 +0500

    Initial commit

---
+++ app/src/main/java/uz/turgunboyevjurabek/brodkeys_gps/MyBroadcastGps.kt
@@ -0,0 +1,12 @@
+package uz.turgunboyevjurabek.brodkeys_gps
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyBroadcastGps : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+}
