11a1f9d20655d363c56f5ecc13f31b09ee228ab1
Asus <turgunboyevjurabek777@gmail.com>
2023-09-11 16:14:39 +0500

    gps finish

--- app/src/main/java/uz/turgunboyevjurabek/brodkeys_gps/MainActivity.kt
+++ app/src/main/java/uz/turgunboyevjurabek/brodkeys_gps/MainActivity.kt
@@ -1,5 +1,9 @@
 package uz.turgunboyevjurabek.brodkeys_gps
 
+import android.app.LocaleManager
+import android.content.Intent
+import android.content.IntentFilter
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myBroadcastGps=MyBroadcastGps()
+        val filter=IntentFilter(LocationManager.PROVIDERS_CHANGED_ACTION)
+        filter.addAction(Intent.ACTION_PROVIDER_CHANGED)
+
+        this.registerReceiver(myBroadcastGps,filter)
+
     }
 }
--- app/src/main/java/uz/turgunboyevjurabek/brodkeys_gps/MyBroadcastGps.kt
+++ app/src/main/java/uz/turgunboyevjurabek/brodkeys_gps/MyBroadcastGps.kt
@@ -3,10 +3,33 @@ package uz.turgunboyevjurabek.brodkeys_gps
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.location.LocationManager
+import android.util.Log
+import android.widget.Toast
 
+private const val TAG = "MyBroadcastGps"
 class MyBroadcastGps : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG,"onReceive : Boshlandi")
 
+        if(LocationManager.PROVIDERS_CHANGED_ACTION.equals(intent.action)){
+            val locationManager=context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+            val isGpsEnabled=locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+            val isNetworkEnabled=locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+            if (isGpsEnabled || isNetworkEnabled){
+                Toast.makeText(context, "gps yoniq", Toast.LENGTH_SHORT).show()
+                Log.d(TAG, "onReceive: gps yondi")
+            }else{
+                Toast.makeText(context, "gps o'chiq", Toast.LENGTH_SHORT).show()
+                Log.d(TAG, "onReceive: gps o'chdi")
+            }
+
+
+        }
     }
+
 }
